dbd4130e24ac7fc4ae73232878e8a6002527c247
Henry <hcewurum@gmail.com>
2019-12-16 16:46:58 -0500

    created an additional component for handling popular games in 2019/any year

---
+++ mgi/src/components/popularIn2019.js
@@ -0,0 +1,48 @@
+import React, { Component } from 'react'
+import { getCat, getPopularIn2019 } from './loadAPI';
+
+class PopularIn2019 extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      porularGames: [],
+      results: '',
+      apiDataLoaded: false
+    }
+  }
+
+  componentDidMount = async () => {
+    let response = await getPopularIn2019()
+    console.log(response);
+    this.setState({
+      result: response,
+      apiDataLoaded: true
+    })
+  }
+
+  hover = () => {
+    // console.log('here');
+
+  }
+
+  render() {
+
+    return (
+
+      <div className="gamesDiv">
+        {this.state.apiDataLoaded &&
+          this.state.result.map((game, index) =>
+            <div key={index} className="gameCatDiv">
+              <h3>{game.name}</h3>
+              <video width="320" height="240" controls className="video" onMouseOver={this.hover}>
+                <source src={game.clip.clips[320]} type="video/mp4" />
+                Your browser does not support the video tag.
+              </video>
+            </div>
+          )}
+      </div>
+    )
+  }
+}
+
+export default PopularIn2019;
