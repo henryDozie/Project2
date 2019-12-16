b357b14856d1d2aa9dfd85a3ac775e13e9939623
Henry <hcewurum@gmail.com>
2019-12-16 13:47:47 -0500

    created the submitbutton to handle search queries in the main page

--- mgi/src/components/mainBody.js
+++ mgi/src/components/mainBody.js
@@ -1,13 +1,18 @@
 import React from 'react';
+import { findGames } from './loadAPI';
 
 const MainBody = (props) => {
   return (
     <form>
       <input
-        type="text" name="0"
+        type="text" name="textarea"
         onChange={props.handleChange}
         placeholder="Find a GameInfo"
       />
+      <input t
+        type="submit"
+        value="submit game"
+        id="gameSearchSubmit" />
     </form>
   )
 }          
