6d8c350f3a0f43eff2a39208a4687df403fc6420
Henry <hcewurum@gmail.com>
2019-12-16 13:46:48 -0500

    edited the first axios call to collect images and titles

--- mgi/src/components/loadAPI.js
+++ mgi/src/components/loadAPI.js
@@ -3,10 +3,10 @@ import axios from 'axios'
 
 export async function getCat() {
   const response = await axios('https://api.rawg.io/api/genres')
-  console.log(response.data.results);
-  return response
+  // console.log(response.data.results);
+  return response.data.results
 }
 
-export async function loadGames() {
-
+export async function findGames(gameName) {
+  
 }
