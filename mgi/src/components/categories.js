b6ca29638c9f47e4528bcfcdf67c554b6fae1415
Henry <hcewurum@gmail.com>
2019-12-16 12:15:08 -0500

    collecting the images and the categories name from the ffirst API call

--- mgi/src/components/categories.js
+++ mgi/src/components/categories.js
@@ -1,11 +1,38 @@
 import Router from 'react-router-dom'
 import React, { Component } from 'react'
+import { getCat } from './loadAPI';
 
-function Categories() {
-  return (
-    <div>
-      Hi, I am Categories Page
-    </div>
-  )
+class Categories extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      gameCategories: []
+    }
+  }
+
+  componentDidMount = async () => {
+    let response = await getCat()
+    this.setState({
+      result: response,
+      apiDataLoaded: true
+    })
+    console.log(response);
+    
+  }
+
+  render() {
+
+    return (
+      <div className="gamesDiv">{this.state.apiDataLoaded &&
+        this.state.result.map((game, index) =>
+          (
+            <div key={index} className="gameCatDiv">
+              <h2>{game.name}</h2>
+              <img src={game.image_background} alt="No Image"/>
+            </div>
+          ))}
+      </div>
+    )
+  }
 }
 export default Categories
