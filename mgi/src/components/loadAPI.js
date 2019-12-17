89f1283afeb9a1880afdb74468ecdb412b7a0d31
Henry <hcewurum@gmail.com>
2019-12-17 12:05:28 -0500

    added an additional apiCall for the search query handling

--- mgi/src/components/loadAPI.js
+++ mgi/src/components/loadAPI.js
@@ -7,6 +7,23 @@ export async function getCat() {
   return response.data.results
 }
 
-export async function findGames(gameName) {
+export async function getPopularIn2019() {
+  const response = await axios('https://api.rawg.io/api/games?dates=2019-01-01,2019-12-31&ordering=-added')
+  // console.log(response);
+  return response.data.results
+}
+
+export async function getHighestRating() {
+  const response = await axios('https://api.rawg.io/api/games?dates=2001-01-01,2001-12-31&ordering=-rating')
+  console.log(response);
+  return response.data.results
+}
+
+export async function getGame(name) {
+  console.log(name)
+  name = name.split(' ').join('-')
+  console.log(name)
   
+  const response = await axios(`https://api.rawg.io/api/games?search=${name}`)
+  return response.data.results
 }
