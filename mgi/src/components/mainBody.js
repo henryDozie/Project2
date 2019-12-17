ae547b364730a7c66c4e3a3eb06418abdbab8c3c
Henry <hcewurum@gmail.com>
2019-12-17 12:03:31 -0500

    added a serch componentfor users to search specific games with response accompanied with similar games

--- mgi/src/components/mainBody.js
+++ mgi/src/components/mainBody.js
@@ -1,20 +1,70 @@
 import React from 'react';
-import { findGames } from './loadAPI';
+import { getGame } from './loadAPI';
 
-const MainBody = (props) => {
-  return (
-    <form>
-      <input
-        type="text" name="textarea"
-        onChange={props.handleChange}
-        placeholder="Find a GameInfo"
-      />
-      <input t
-        type="submit"
-        value="submit game"
-        id="gameSearchSubmit" />
-    </form>
-  )
-}          
+class MainBody extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      searchResponse: [],
+      apiDataLoaded: false
+    }
+  }
+
+  // componentDidMount = async () => {
+  //   let response = await getGame()
+  //   this.setState({
+  //     result: response,
+  //     apiDataLoaded: true
+  //   })
+  //   console.log(response); 
+  // }
+
+  onSubmit = async (e) => {
+    e.preventDefault()
+    const name = this.state.searchResponse
+    const result = await getGame(name)
+    console.log(result);
+    
+    this.setState({
+      searchResponse: result,
+      apiDataLoaded: true
+    })
+  }
   
+  onChange = (e) => {
+    const name = e.target.value;
+    console.log(name);
+    
+    this.setState({
+      searchResponse: name
+    })
+  }
+
+
+  render() {
+    return (
+      <div className="gamesDiv">
+        <form
+          id="searchForm"
+          onSubmit={this.onSubmit}>
+          <input
+            type="text"
+            name="gameName"
+            id="searchInput"
+            placeholder="Game Info"
+            onChange={this.onChange}></input>
+        </form>
+        {this.state.apiDataLoaded &&
+        this.state.searchResponse.map((game, index) =>
+          (
+            <div key={index} className="gameCatDiv">
+              <h2>{game.name}</h2>
+              <img src={game.background_image} alt="No Image"/>
+            </div>
+          ))}
+
+      </div>
+    )
+  }
+}
 export default MainBody
