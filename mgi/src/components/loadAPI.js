f0993e94f6a9b58c16d315fe586a0622c7b375a1
Henry <hcewurum@gmail.com>
2019-12-18 10:23:33 -0500

    Added two api calls for new data(last30Days and Highest Rating)

--- mgi/src/components/loadAPI.js
+++ mgi/src/components/loadAPI.js
@@ -1,4 +1,3 @@
-import React from 'react'
 import axios from 'axios'
 
 export async function getCat() {
@@ -9,21 +8,40 @@ export async function getCat() {
 
 export async function getPopularIn2019() {
   const response = await axios('https://api.rawg.io/api/games?dates=2019-01-01,2019-12-31&ordering=-added')
-  // console.log(response);
   return response.data.results
 }
 
 export async function getHighestRating() {
   const response = await axios('https://api.rawg.io/api/games?dates=2001-01-01,2001-12-31&ordering=-rating')
-  console.log(response);
   return response.data.results
 }
 
 export async function getGame(name) {
-  console.log(name)
   name = name.split(' ').join('-')
-  console.log(name)
-  
   const response = await axios(`https://api.rawg.io/api/games?search=${name}`)
+  return response.data.results
+}
+
+export async function getLast30Games() {
+  const n = new Date()
+  const year = n.getFullYear()
+  const month = n.getMonth()
+  const day = n.getDate()
+  const prevMonth = month - 1
+  let prevYear;
+  if (month === 0) {
+    prevYear = year;
+  }
+  else {
+    prevYear = year
+  }
+  const response = await axios(`https://api.rawg.io/api/games?dates=${prevYear}-${prevMonth}-${day},${year}-${month}-${day}&platforms=18,1,7`)
+  return response.data.results
+}
+
+export async function getAllTimeGreats() {
+  const response = await axios(`https://api.rawg.io/api/games?ordering=-rating&developers=109`)
+  console.log(response.data.results);
+  
   return response.data.results
 }
