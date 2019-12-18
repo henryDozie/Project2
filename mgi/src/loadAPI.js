631ddb48b1a1d61587874c6cb0aa1acf58a95d35
Henry <hcewurum@gmail.com>
2019-12-18 14:04:18 -0500

    Debugged the whole code

--- mgi/src/loadAPI.js
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-import axios from 'axios'
-
-export async function LoadApi() {
-  const result = await axios('https://api.rawg.io/api/games')
-
-  console.log(result);
-  return result
-}
