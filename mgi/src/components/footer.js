71ff7015ce4f0873ca000b4770f42024f553bc68
Henry <hcewurum@gmail.com>
2019-12-20 00:24:11 -0500

    added a footer component with icons

---
+++ mgi/src/components/footer.js
@@ -0,0 +1,18 @@
+import React from "react";
+
+function Footer() {
+  return (
+    <div className="footer">
+      <footer>
+        <div id="footerIcons">
+          <div className="Indicon"><a href="https://github.com/henryDozie/Project2/tree/master/mgi"><i class="fab fa-github fa-2x"></i></a></div>
+          <div><i class="fas fa-phone fa-2x"></i></div>
+          <div><i class="far fa-envelope fa-2x"></i></div>
+          <div className="Indicon"><a href="www.linkedin.com/in/henryewurum"><i class="fab fa-linkedin fa-2x"></i></a></div>
+        </div>
+        <div id="copyright"><i class="far fa-copyright fa-1x"></i> H.C. Ewurum 2019</div>
+      </footer>
+    </div>
+  );
+}
+export default Footer;
