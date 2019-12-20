47678eb5b476daaa0b247a35a09252642c5ba9e3
Henry <hcewurum@gmail.com>
2019-12-19 19:56:00 -0500

    Added the game card with props to this component

--- mgi/src/components/last30Days.js
+++ mgi/src/components/last30Days.js
@@ -1,45 +1,46 @@
-import React from 'react'
-import { getLast30Games } from './loadAPI';
+import React from "react";
+import { getLast30Games } from "./loadAPI";
+import GameCard from "./gameCard";
 
 class Last30Days extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
       porularGames: [],
-      results: '',
+      results: "",
       apiDataLoaded: false
-    }
+    };
   }
 
   componentDidMount = async () => {
-    let response = await getLast30Games()
+    let response = await getLast30Games();
     console.log(response);
     this.setState({
       result: response,
       apiDataLoaded: true
-    })
-  }
-
-  hover = () => {
-    // console.log('here');
-
-  }
+    });
+  };
 
   render() {
-
+    console.log(this.state.result);
     return (
-
-      <div className="gamesDiv">{this.state.apiDataLoaded &&
-        this.state.result.map((game, index) =>
-          (
-            <div key={index} className="gameCatDiv">
-              <h2>{game.name}</h2>
-              <img src={game.background_image} alt=""/>
-            </div>
+      <div className="gamesDiv">
+        {this.state.apiDataLoaded &&
+          this.state.result.map((game, index) => (
+            <GameCard
+              name={game.name}
+              background_image={game.background_image}
+              released={game.released}
+              rating={game.rating}
+              ratings_count={game.ratings_count}
+              suggestions_count={game.suggestions_count}
+              index={index}
+              handleClick={this.props.handleClick}
+              game={game}
+            />
           ))}
-        
       </div>
-    )
+    );
   }
 }
 
