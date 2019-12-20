0a18adf9faa64da4e6d5b006ff75c139c9abd95d
Henry <hcewurum@gmail.com>
2019-12-19 19:52:48 -0500

    finished the functionality for the categories component

--- mgi/src/components/categories.js
+++ mgi/src/components/categories.js
@@ -1,36 +1,37 @@
-import React from 'react'
-import { getCat } from './loadAPI';
+import React from "react";
+import { getCat } from "./loadAPI";
 
 class Categories extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
       gameCategories: []
-    }
+    };
   }
 
   componentDidMount = async () => {
-    let response = await getCat()
+    let response = await getCat();
     this.setState({
       result: response,
       apiDataLoaded: true
-    })
-    
-  }
+    });
+  };
 
   render() {
-
     return (
-      <div className="gamesDiv">{this.state.apiDataLoaded &&
-        this.state.result.map((game, index) =>
-          (
+      <div className="gamesDiv">
+        {this.state.apiDataLoaded &&
+          this.state.result.map((game, index) => (
             <div key={index} className="gameCatDiv">
               <h2>{game.name}</h2>
-              <img src={game.image_background} alt=""/>
+              <img className="catImages" src={game.image_background} alt="" />
+              <div className="gameCatDesc">
+                Games Count: {game.games_count.toLocaleString()}
+              </div>
             </div>
           ))}
       </div>
-    )
+    );
   }
 }
-export default Categories
+export default Categories;
