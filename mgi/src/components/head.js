f407e6d2d535fa53f5cdf907955a0092ca462385
Henry <hcewurum@gmail.com>
2019-12-20 10:26:16 -0500

    Added sliding images to the landing page of the website

--- mgi/src/components/head.js
+++ mgi/src/components/head.js
@@ -2,8 +2,20 @@ import React from "react";
 
 function Head() {
   return (
-    <div className="headerRegion">
-      <img src="https://imgur.com/CXVb4lv.png" alt="none"></img>
+    <div>
+      <div className="headerRegion">
+        <img src="https://imgur.com/CXVb4lv.png" alt="none"></img>
+      </div>
+      <div className="Headpic">
+      <img src="https://imgur.com/wFW9sBY.jpg" alt="none"></img>
+      <img src="https://imgur.com/BcOdZrH.jpg" alt="none"></img>
+      <img src="https://imgur.com/ir2XyMP.jpg" alt="none"></img>
+      <img src="https://imgur.com/mCUEOaa.jpg" alt="none"></img>
+      <img src="https://imgur.com/hTBCbo3.jpg" alt="none"></img>
+      <img src="https://imgur.com/YxvUy7A.jpg" alt="none"></img>
+      <img src="https://imgur.com/vcWq4Lr.jpg" alt="none"></img>
+      
+      </div>
     </div>
   );
 }
