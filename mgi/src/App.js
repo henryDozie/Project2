81af64e62fca4a5dbc19c3cd580db47bb0f87b45
Henry <hcewurum@gmail.com>
2019-12-19 20:00:43 -0500

    created more routes

--- mgi/src/App.js
+++ mgi/src/App.js
@@ -1,38 +1,66 @@
-import React from 'react';
-import './App.css'
-import Header from './components/header'
-import { Route } from 'react-router-dom';
-import Categories from './components/categories'
-import HighestRating from './components/highestRating';
-import MainBody from './components/mainBody';
-import PopularIn2019 from './components/popularIn2019'
-import Last30Days from './components/last30Days'
-import AllTimeGreats from './components/allTimeGreats';
-import ThisWeek from './components/thisWeek';
-import Platforms from './components/platforms';
+import React from "react";
+import "./App.css";
+import Header from "./components/header";
+import { Route } from "react-router-dom";
+import Categories from "./components/categories";
+import HighestRating from "./components/highestRating";
+import Search from "./components/search";
+import PopularIn2019 from "./components/popularIn2019";
+import Last30Days from "./components/last30Days";
+import AllTimeGreats from "./components/allTimeGreats";
+import ThisWeek from "./components/thisWeek";
+import Platforms from "./components/platforms";
+import HomeBody from "./components/homeBody";
+import MyLibrary from "./components/myLibrary";
 
 class App extends React.Component {
   constructor(props) {
     super(props);
 
     this.state = {
-      
-    }
+      favorites: []
+    };
   }
+
+  handleClick = (e, results) => {
+    e.preventDefault();
+    let favFlag = true;
+    let newFavorite = this.state.favorites;
+    for (let i = 0; i < newFavorite.length; i++) {
+      if (newFavorite[i].id === results.id) {
+        favFlag = false;
+        break;
+      }
+    }
+    if (favFlag) {
+      newFavorite.push(results);
+    }
+    this.setState({
+      favorites: newFavorite
+    });
+  };
 
   render() {
     return (
       <div className="App">
-        <Header />        
-        <Route path="/categories" render={() => <Categories/>}/>
-        <Route path="/popularIn2019" render={() => <PopularIn2019/>}/>
-        <Route path="/last30Days" render={() => <Last30Days/>}/>
-        <Route path="/mainBody" render={() => <MainBody/>}/>
-        <Route path="/allTimeGreats" render={() => <AllTimeGreats />} />
+        <Header />
+        <Route path="/categories" render={() => <Categories />} />
+        <Route path="/popularIn2019" render={() => <PopularIn2019 handleClick={this.handleClick}/>} />
+        <Route path="/last30Days" render={() => <Last30Days handleClick={this.handleClick}/>} />
+        <Route path="/search" render={() => <Search handleClick={this.handleClick}/>} />
+        <Route
+          path="/allTimeGreats"
+          render={() => <AllTimeGreats handleClick={this.handleClick} />}
+        />
         <Route path="/thisWeek" render={() => <ThisWeek />} />
         <Route path="/platforms" render={() => <Platforms />} />
-        <Route path="/highestRating" render={() => <HighestRating />} />
-       />
+        <Route path="/highestRating" render={() => <HighestRating handleClick={this.handleClick}/>} />
+        <Route
+          path="/myLibrary"
+          render={() => <MyLibrary favorites={this.state.favorites} />}
+        />
+        />
+        <HomeBody />
       </div>
     );
   }
