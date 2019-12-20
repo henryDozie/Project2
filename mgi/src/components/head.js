f3259126ce64b068237b182eaaeaddeace187884
Henry <hcewurum@gmail.com>
2019-12-20 00:23:47 -0500

    added a head component

---
+++ mgi/src/components/head.js
@@ -0,0 +1,10 @@
+import React from "react";
+
+function Head() {
+  return (
+    <div className="headerRegion">
+      <img src="https://imgur.com/CXVb4lv.png" alt="none"></img>
+    </div>
+  );
+}
+export default Head;
