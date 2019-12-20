8591d273a8bbc9b37336e9e8c7f44f825e3c18be
Henry <hcewurum@gmail.com>
2019-12-19 19:58:34 -0500

    Created a this component for collecting the game platforms from the new api call

--- mgi/src/components/platforms.js
+++ mgi/src/components/platforms.js
@@ -1,5 +1,6 @@
-import React from 'react'
-import { getPlatforms } from './loadAPI';
+import React from "react";
+import { getPlatforms } from "./loadAPI";
+import PlatformCard from "./platformCard";
 
 class Platforms extends React.Component {
   constructor(props) {
@@ -8,52 +9,31 @@ class Platforms extends React.Component {
       gameCategories: [],
       result: [],
       apiDataLoaded: false
-    }
+    };
   }
 
   componentDidMount = async () => {
-    let response = await getPlatforms()
+    let response = await getPlatforms();
     this.setState({
       result: response,
       apiDataLoaded: true
-    })
-
-  }
+    });
+  };
 
   render() {
-
     return (
       <div className="gamesDiv">
         {this.state.apiDataLoaded &&
-          this.state.result.map((game, index) =>
-            <div key={index} className="gameCatDiv">
-              <i className="fas fa-plus-circle fa-2x"></i>
-              <div className="catHead">
-                <h4 className="gameName">{game.name}</h4>
-              </div>
-              <img src={game.image_background} alt="gameInfoImage"></img>
-              <div className="info">
-                <div>
-                  <ul>
-
-                    <li><h2>{game.games[0].name}</h2></li>
-                    <li><h2>{game.games[1].name}</h2></li>
-                    <li><h2>{game.games[2].name}</h2></li>
-                    <li><h2>{game.games[3].name}</h2></li>
-                    <li><h2>{game.games[4].name}</h2></li>
-                    <li><h2>{game.games[5].name}</h2></li>
-
-                    <li><div className="platforms"><h2>Platform:</h2>   <i className="fab fa-xbox"></i><i class="fab fa-playstation"></i><i class="fab fa-steam"></i><i class="fas fa-laptop"></i></div></li>
-
-                  </ul>
-                </div>
-              </div>
-
-            </div>
-          )}
+          this.state.result.map((game, index) => (
+            <PlatformCard
+              name={game.name}
+              image_background={game.image_background}
+              index={index}
+              games={game.games}
+            />
+          ))}
       </div>
-    )
+    );
   }
 }
-export default Platforms
-
+export default Platforms;
