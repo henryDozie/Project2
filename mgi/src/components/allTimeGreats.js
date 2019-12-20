23d41c89ce76ea0825d2912ffac6c725db196d02
Henry <hcewurum@gmail.com>
2019-12-19 19:52:17 -0500

    finished the functionality for the all time greats component

--- mgi/src/components/allTimeGreats.js
+++ mgi/src/components/allTimeGreats.js
@@ -1,50 +1,42 @@
-import React from 'react'
-import { getAllTimeGreats } from './loadAPI';
-import GameCard from './gameCard';
+import React from "react";
+import { getAllTimeGreats } from "./loadAPI";
+import GameCard from "./gameCard";
 
 class AllTimeGreats extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
       greats: [],
-      results: '',
+      results: "",
       apiDataLoaded: false,
       hovered: false
-    }
+    };
   }
 
   componentDidMount = async () => {
-    let response = await getAllTimeGreats()
+    let response = await getAllTimeGreats();
     console.log(response);
     this.setState({
       greats: response,
       apiDataLoaded: true
-    })
-  }
-
-  showDropdown = () => {
-    console.log("hovered")
-    this.setState({
-      hovered: true
-    })
-  }
-  hideDropDown = () => {
-    this.setState({
-      hovered: false
-    })
-  }
+    });
+  };
 
   render() {
-
     return (
-
       <div className="gamesDiv">
         {this.state.apiDataLoaded &&
-          this.state.greats.map((game, index) =>
-            <GameCard name={game.name} background_image={game.background_image} index={index}/>
-          )}
+          this.state.greats.map((game, index) => (
+            <GameCard
+              name={game.name}
+              background_image={game.background_image}
+              index={index}
+              handleClick={this.props.handleClick}
+              game={game}
+            />
+          ))}
       </div>
-    )
+    );
   }
 }
 
